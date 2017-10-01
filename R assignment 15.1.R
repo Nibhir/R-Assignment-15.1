35314ed772b296d6f786eaa302f576ccf36f540d
Nibhir Ghosh <30548445+Nibhir@users.noreply.github.com>
2017-10-01 16:43:49 +0530

    Add files via upload

---
+++ R assignment 15.1.R
@@ -0,0 +1,12 @@
+install.packages("xlsx")
+
+library("xlsx")
+
+CSV <- read.csv("C:/Users/acer/Desktop/CSV.csv")
+
+xlsx <- read_excel("C:/Users/acer/Desktop/xlsx.xlsx")
+
+txt<-read.table("C:/Users/acer/Desktop/txt.txt")
+
+
+
